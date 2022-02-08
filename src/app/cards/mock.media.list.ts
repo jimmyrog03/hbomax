e13214161fbdfda93e914c5ce65385e7add2f842
jimmyrog03 <jimmyrog03@gmail.com>
2022-02-08 17:52:06 -0500

    Added Media to Fill Carousels to Full for Single Row, Will need more Lists to include unique ones for different slides

--- src/app/cards/mock.media.list.ts
+++ src/app/cards/mock.media.list.ts
@@ -22,5 +22,53 @@ export const mock_media_list = [
         Xtall_img: "./assets/Malignant_XTall.jpeg",
         title: "Regular Show",
         subtitle_HBO: " "
+    },
+    {
+        square_img: "./assets/RickAndMorty_Square.jpeg",
+        wide_img: "./assets/RickAndMorty_Wide.jpeg",
+        tall_img: "./assets/RickAndMorty_Tall.jpeg",
+        Xtall_img: "./assets/Malignant_XTall.jpeg",
+        title: "Rick And Morty",
+        subtitle_HBO: " "
+    },
+    {
+        square_img: "./assets/AdventureTime_Square.jpeg",
+        wide_img: "./assets/AdventureTime_Wide.jpeg",
+        tall_img: "./assets/AdventureTime_Tall.jpeg",
+        Xtall_img: "./assets/Malignant_XTall.jpeg",
+        title: "Adventure Time",
+        subtitle_HBO: " "
+    },
+    {
+        square_img: "./assets/SpaceOdyssey_Square.jpeg",
+        wide_img: " ",
+        tall_img: "./assets/SpaceOdyssey_Tall.jpeg",
+        Xtall_img: "./assets/Malignant_XTall.jpeg",
+        title: "2001: A Space Odyssey",
+        subtitle_HBO: " "
+    },
+    {
+        square_img: "./assets/Memento_Square.jpeg",
+        wide_img: " ",
+        tall_img: "./assets/Memento_Tall.jpeg",
+        Xtall_img: " ",
+        title: "Memento",
+        subtitle_HBO: "HBO"
+    },
+    {
+        square_img: "./assets/Godzilla_Square.jpeg",
+        wide_img: " ",
+        tall_img: "./assets/Godzilla_Tall.jpeg",
+        Xtall_img: " ",
+        title: "Godzilla",
+        subtitle_HBO: " "
+    },
+    {
+        square_img: "./assets/Goonies_Square.jpeg",
+        wide_img: " ",
+        tall_img: "./assets/Goonies_Tall.jpeg",
+        Xtall_img: " ",
+        title: "The Goonies",
+        subtitle_HBO: " "
     }
 ];
